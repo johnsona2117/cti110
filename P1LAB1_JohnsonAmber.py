19a3f454df9c7b426f53c877fcd94767b27326d7
johnsona2117 <160448867+johnsona2117@users.noreply.github.com>
2024-02-18 22:11:29 -0500

    First Submission

---
+++ P1LAB1_JohnsonAmber.py
@@ -0,0 +1,14 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # Amber Johnson
+>>> # 18 FEB 2024
+>>> # P1LAB1
+>>> # python input and outputs
+>>> print('Enter first name', end=' ')
+Enter first name 
+>>> first_name= input()
+
+>>> print('Hey', first_name)
+Hey 
+>>> print('Welcome to zyBooks!')
+Welcome to zyBooks!
